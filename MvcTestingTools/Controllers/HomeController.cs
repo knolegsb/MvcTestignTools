5dd5299bb25b2adbbc172c6617506f2d4d0abd02
knolegsb <seon2you@hotmail.com>
2015-10-28 16:34:29 -0700

    added interface

--- MvcTestingTools/Controllers/HomeController.cs
+++ MvcTestingTools/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcTestingTools.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,20 @@ namespace MvcTestingTools.Controllers
 {
     public class HomeController : Controller
     {
+        private Product[] products =
+        {
+            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
+            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
+            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
+            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
+        };
+
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
         }
     }
 }
